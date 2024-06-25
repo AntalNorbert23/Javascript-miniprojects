e53354bfad6e543183d61658e0598135b81cb9c9
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-06-25 15:32:53 +0300

    added route communication and history preserving

--- SPA/frontend/static/js/index.js
+++ SPA/frontend/static/js/index.js
@@ -1,10 +1,18 @@
+import Dashboard from "./views/Dashboard.js";
+import Posts from "./views/Posts.js";
+import Settings from "./views/Settings.js";
+
+const navigateTo=url=>{
+    history.pushState(null,null,url);
+    router();
+}
 
 const router=async()=>{
     const routes=[
-        {path:"/", view:()=>console.log("viewing dashboard")},
-        {path:"/posts", view:()=>console.log("viewing posts")},
-        {path:"/settings", view:()=>console.log("viewing settings")},
-        {path:"/404",view:()=>{console.log("viewing 404")}}
+        {path:"/", view: Dashboard},
+        {path:"/posts", view:Posts},
+        {path:"/settings", view:Settings},
+        //{path:"/404",view:()=>{console.log("viewing 404")}}
     ];
 
     //test units for a potential route-loop through
@@ -22,9 +30,22 @@ const router=async()=>{
         }
     }
 
+    const view = new match.route.view();
+
+    document.querySelector('#app').innerHTML=await view.getHtml();
     console.log(match)
 };
 
+//history preserving
+window.addEventListener("popstate",router);
+
+//route navigation
 document.addEventListener("DOMContentLoaded",()=>{
+    document.body.addEventListener("click",(event)=>{
+        if(event.target.matches("[data-link]")){
+            event.preventDefault();
+            navigateTo(event.target.href);
+        }
+    })
     router();
 })
