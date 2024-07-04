17bec6a39dcf8dd1e7bf29c3b8d3e54a84735956
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-07-04 23:51:33 +0300

    added songs and possibility to change songs(forward-backward)

--- Music_player/index.js
+++ Music_player/index.js
@@ -1,12 +1,11 @@
+import songs from "./songs.js";
 
 const progress=document.getElementById("progress");
 const song=document.getElementById("song");
 const controlIcon=document.getElementById("controlIcon");
 const controlIconContainer=document.getElementById("controlIconContainer")
-song.onloadedmetadata=function(){
-    progress.max=song.ondurationchange;
-    progress.value=song.currentTime;
-}
+let currentSongIndex=0;
+
 
 controlIconContainer.addEventListener('click',function(){
     if(controlIcon.classList.contains("fa-pause")){
@@ -15,18 +14,45 @@ controlIconContainer.addEventListener('click',function(){
         controlIcon.classList.add("fa-play");
     }else{
         song.play();
+        if(song.play()){
+            setInterval(()=>{
+                progress.value=song.currentTime;
+            },1000)
+        }
         controlIcon.classList.add("fa-pause");
         controlIcon.classList.remove("fa-play");
     }
 })
 
-if(song.play()){
-    setInterval(()=>{
-        progress.value=song.currentTime;
-    },500)
-}
 
 progress.onchange=function(){
     song.play();
     song.currentTime=progress.value;
 }
+
+function loadSong(index){
+    const songData=songs[index];
+    document.querySelector('.song-title').textContent = songData.title;
+    document.querySelector('.artist').textContent = songData.artist;
+    document.querySelector('.song-img').src = songData.img;
+    song.src=songData.src;
+    song.onloadedmetadata=()=>{
+        progress.max=song.duration;
+        progress.value=song.currentTime;
+    }
+}
+
+document.querySelector('.backward-container').addEventListener('click',()=> {
+    currentSongIndex=(currentSongIndex-1+songs.length) % songs.length
+    loadSong(currentSongIndex);
+    song.play();
+})
+
+document.querySelector('.forward-container').addEventListener('click',()=>{
+    currentSongIndex=(currentSongIndex +1) % songs.length;
+    loadSong(currentSongIndex);
+    song.play();
+})
+
+
+loadSong(currentSongIndex);
---
+++ Music_player/songs.js
@@ -0,0 +1,22 @@
+const songs = [
+    {
+        title: "Despacito",
+        artist: "Luis Fonsi Ft. Daddy Yankee",
+        src: "musics/mp3/despacito.mp3",
+        img: "musics/thumbnails/despacito.jpeg"
+    },
+    {
+        title: "Shape of You",
+        artist: "Ed Sheeran",
+        src: "musics/mp3/shapeOfYou.mp3",
+        img:  "musics/thumbnails/shapeOfYou.png"
+    },
+    {
+        title: "Blinding Lights",
+        artist: "The Weeknd",
+        src: "musics/mp3/blindingLights.mp3",
+        img: "musics/thumbnails/blindingLights.png"
+    }
+];
+
+export default songs
