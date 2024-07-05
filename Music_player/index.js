cf977385e6df9559884467d2aacb20dea2c3d31f
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-07-05 23:18:08 +0300

    added audio volume control and key event controls

--- Music_player/index.js
+++ Music_player/index.js
@@ -3,9 +3,12 @@ import songs from "./songs.js";
 const progress=document.getElementById("progress");
 const song=document.getElementById("song");
 const controlIcon=document.getElementById("controlIcon");
-const controlIconContainer=document.getElementById("controlIconContainer")
+const controlIconContainer=document.getElementById("controlIconContainer");
+const volumeControlContainer=document.getElementById("volume-control-container");
+const volumeControl=document.getElementById("volume-control");
 let currentSongIndex=0;
-
+let lastKeyTime=0;
+const doublePressDelay=300;
 
 controlIconContainer.addEventListener('click',function(){
     if(controlIcon.classList.contains("fa-pause")){
@@ -52,6 +55,47 @@ document.querySelector('.forward-container').addEventListener('click',()=>{
     currentSongIndex=(currentSongIndex +1) % songs.length;
     loadSong(currentSongIndex);
     song.play();
+})
+
+
+document.querySelector(".circle-left").addEventListener('click',()=>{
+    if(volumeControlContainer.style.display ==="none"){
+        volumeControlContainer.style.display="block";
+    }else{
+        volumeControlContainer.style.display="none";
+    }
+})
+
+volumeControl.addEventListener('input',()=>{
+    song.volume=volumeControl.value;
+})
+
+document.addEventListener('keydown',(event)=>{
+    let key=event.key;
+
+    if(key === "ArrowDown"){
+        song.volume=Math.max(0, song.volume - 0.1);
+    }else if( key === "ArrowUp"){
+        song.volume=Math.min(1, song.volume + 0.1);
+    }else if ( key === "ArrowRight"){
+        let currentTime=new Date().getTime();
+        if(currentTime-lastKeyTime<doublePressDelay){
+            currentSongIndex=(currentSongIndex+1)%songs.length;
+            loadSong(currentSongIndex);
+        }
+        lastKeyTime=currentTime;
+        song.play();
+    }else if( key === "ArrowLeft"){
+        let currentTime= new Date().getTime();
+        if(currentTime-lastKeyTime<doublePressDelay){
+            currentSongIndex=(currentSongIndex-1+songs.length) % songs.length;
+            loadSong(currentSongIndex);
+        }
+        lastKeyTime=currentTime;
+        song.play();
+    }
+
+    volumeControl.value = song.volume;
 })
 
 
